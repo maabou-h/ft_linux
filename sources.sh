61591f76cacf259eb785377492335762abf08974
maabou-h <48495902+maabou-h@users.noreply.github.com>
2020-04-28 18:44:48 +0200

    Update sources.sh

--- sources.sh
+++ sources.sh
@@ -3,7 +3,7 @@
 mkdir -v $LFS/sources
 chmod -v a+wt $LFS/sources
 
-wget https://raw.githubusercontent.com/maabou-h/ft_linux/master/wget-list
+wget --no-check-certificate https://raw.githubusercontent.com/maabou-h/ft_linux/master/wget-list
 
 wget --input-file=wget-list --continue --directory-prefix=$LFS/sources
 
