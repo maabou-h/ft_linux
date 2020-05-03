5859f441cc299935affbd4a32dc7c2179542590c
maabou-h <48495902+maabou-h@users.noreply.github.com>
2020-05-03 18:38:43 +0200

    Create glibc.sh

---
+++ glibc.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd $LFS/sources
+
+tar -xf glibc-2.22.tar.xz
+
+cd glibc-2.22
+
+mkdir -v ../glibc-build
+cd ../glibc-build
+
+../glibc-2.22/configure                             \
+      --prefix=/tools                               \
+      --host=$LFS_TGT                               \
+      --build=$(../glibc-2.22/scripts/config.guess) \
+      --disable-profile                             \
+      --enable-kernel=2.6.32                        \
+      --enable-obsolete-rpc                         \
+      --with-headers=/tools/include                 \
+      libc_cv_forced_unwind=yes                     \
+      libc_cv_ctors_header=yes                      \
+      libc_cv_c_cleanup=yes
+      
+make
+
+make install
